0c4e5b3fe5b3fee829e899242acb0db57bb1fd82
productshan <shandypratama.production@gmail.com>
2023-10-18 13:29:15 +0700

    Change again

--- src/components/modal/ClientOnlyPortal.tsx
+++ src/components/modal/ClientOnlyPortal.tsx
@@ -5,12 +5,13 @@ import { createPortal } from "react-dom";
 export default function ClientOnlyPortal({ children, selector }: { children: React.ReactNode; selector: string }) {
   var ref = useRef<HTMLDivElement | null>(null);
   const [mounted, setMounted] = useState(false);
-  if (ref.current) {
-    useEffect(() => {
-      ref.current = document.querySelector(selector);
-      setMounted(true);
-    }, [selector]);
 
+  useEffect(() => {
+    ref.current = document.querySelector(selector);
+    setMounted(true);
+  }, [selector]);
+
+  if (ref.current) {
     return mounted ? createPortal(children, ref.current) : null;
   }
 }
