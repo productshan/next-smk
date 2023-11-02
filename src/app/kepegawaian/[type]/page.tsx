936b6350fa1413bc9233a20307c4dc8c4bec8d1c
productshan <shandypratama.production@gmail.com>
2023-11-02 14:32:19 +0700

    Adding title for each section.

--- src/app/kepegawaian/[type]/page.tsx
+++ src/app/kepegawaian/[type]/page.tsx
@@ -12,16 +12,21 @@ export default function Index({ params }: { params: any }) {
   }
 
   return (
-    <div className="flex grid grid-cols-2 gap-4 lg:grid-cols-4 lg:gap-6 xl:grid-cols-4 xl:gap-8">
-      {employee.map((profiles, key) => {
-        if (profiles.status === type) {
-          return (
-            <div key={key} className="w-full h-full flex items-center justify-center">
-              <Card profile={profiles} />
-            </div>
-          );
-        }
-      })}
+    <div className="w-full">
+      <p className="text-center text-2xl my-4 font-semibold">{`Daftar ${
+        type === "guru" ? "Guru" : "Tenaga Pendidik"
+      } SMK Negeri 1 Banyuasin III Tahun 2023/2024`}</p>
+      <div className="flex grid grid-cols-2 gap-4 lg:grid-cols-4 lg:gap-6 xl:grid-cols-4 xl:gap-8">
+        {employee.map((profiles, key) => {
+          if (profiles.status === type) {
+            return (
+              <div key={key} className="w-full h-full flex items-center justify-center">
+                <Card profile={profiles} />
+              </div>
+            );
+          }
+        })}
+      </div>
     </div>
   );
 }
